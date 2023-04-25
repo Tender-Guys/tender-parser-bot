555b3ee41a50759bccc1d2d88946254225abdf32
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2023-04-25 20:43:33 +0300

    basic bot initialization

---
+++ src/main/java/project/bot/config/BotInitializer.java
@@ -0,0 +1,42 @@
+package project.bot.config;
+
+/**
+ * @author Vladyslav Pustovalov
+ * class which registers the created bot
+ */
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import project.bot.service.TenderParserServiceBot;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BotInitializer {
+
+    final BotConfig config = new BotConfig();
+
+    final TenderParserServiceBot bot = new TenderParserServiceBot(config.getBotOptions(), config.getBotToken());
+
+    /**
+     * Method which registers the created bot
+     */
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: "+e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/project/bot/service/TenderParserServiceBot.java
@@ -0,0 +1,102 @@
+package project.bot.service;
+/**
+ * @author Vladyslav Pustovalov
+ * class TelegramBot which gets messages from useers and could answer to them
+ */
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import project.bot.config.BotConfig;
+
+@Component
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TenderParserServiceBot extends TelegramLongPollingBot{
+    final BotConfig config = new BotConfig();
+
+    final MessageSender messageSender = new MessageSender();
+
+    public TenderParserServiceBot(DefaultBotOptions options, String botToken) {
+        super(options, botToken);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    /**
+     * Method which gets updates from users and sends to them an answer
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        String userName = update.getMessage().getChat().getUserName();
+        String messageText = update.getMessage().getText();
+        long chatId = update.getMessage().getChatId();
+        String errorOccurred = "Error occurred: ";
+
+        if (!update.hasMessage() || !update.getMessage().hasText()) {
+            log.warn("Unexpected update from user " + userName);
+            try {
+                execute(messageSender.warnMessage(chatId));
+                log.info("Warn message was sent to user " + userName);
+            } catch (TelegramApiException e) {
+                log.error(errorOccurred + e);
+            }
+        } else {
+
+            switch (messageText) {
+                case "/start" -> {
+                    try {
+                        execute(messageSender.startMessage(chatId));
+                        log.info("Welcome message was sent to user " + userName);
+                    } catch (TelegramApiException e) {
+                        log.error(errorOccurred + e);
+                    }
+                }
+
+                case "My subscription list" -> {
+                    try {
+                        execute(messageSender.userSubscriptionsList(chatId));
+                        log.info("Subscription list was sent to user " + userName);
+                    } catch (TelegramApiException e) {
+                        log.error(errorOccurred + e);
+                    }
+                }
+
+                case "Available tender sites for subscription" -> {
+                    try {
+                        execute(messageSender.availableSitesList(chatId));
+                        log.info("Available sites list was sent to user " + userName);
+                    } catch (TelegramApiException e) {
+                        log.error(errorOccurred + e);
+                    }
+                }
+
+                case "Help instructions" -> {
+                    try {
+                        execute(messageSender.helpMessage(chatId));
+                        log.info("Help instructions was sent to user " + userName);
+                    } catch (TelegramApiException e) {
+                        log.error(errorOccurred + e);
+                    }
+                }
+
+                default -> {
+                    try {
+                        execute(messageSender.defaultMessage(chatId));
+                        log.info("Default message was sent to user " + userName);
+                    } catch (TelegramApiException e) {
+                        log.error(errorOccurred + e);
+                    }
+                }
+            }
+        }
+    }
+}
