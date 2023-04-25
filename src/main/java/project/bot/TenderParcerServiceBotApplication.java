a1276872b6cf9f2806088b877ddd8574ab1c0cc6
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2023-04-25 20:27:38 +0300

    init commit

---
+++ src/main/java/project/bot/TenderParcerServiceBotApplication.java
@@ -0,0 +1,13 @@
+package project.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderParcerServiceBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenderParcerServiceBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/bot/TenderParcerServiceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package project.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenderParcerServiceBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
