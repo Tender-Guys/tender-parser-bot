9997807f0a022fe9ffb7558cc6619a8e49b371e6
Vladyslav Pustovalov <pustovalov.v.y@gmail.com>
2023-06-18 13:51:53 +0300

    cleanup

--- src/test/java/project/bot/TenderParcerServiceBotApplicationTests.java
+++ src/test/java/project/bot/TenderParcerServiceBotApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TenderParcerServiceBotApplicationTests {
+class TenderParserServiceBotApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void shouldAnswerWithTrue(){
+		assert (true);
 	}
-
 }
